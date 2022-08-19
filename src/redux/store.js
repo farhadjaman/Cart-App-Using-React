3db358cebe5c29651b927374ec60eb9d64497a00
Md Farhad Jaman <www.farhadjaman11@gmail.com>
2022-08-19 12:49:43 +0600

    prediciting state after reducing the actions using middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,6 +7,9 @@ const myLogger = (store) => (next) => (action) => {
   console.log(`Action:${JSON.stringify(action)}`);
   console.log(`Before: ${JSON.stringify(store.getState())}`)
 
+  const upcommingState = [action].reduce(rootReducer, store.getState())
+  console.log("After :", JSON.stringify(upcommingState))
+
   //let the action passed to the reducer
   return next(action);
 }
